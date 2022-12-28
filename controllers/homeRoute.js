713755ded6633682eb5528946246f2725ad028ae
Yigit Ocak <yigitocak@Yigits-MacBook-Pro.local>
2022-12-28 15:39:07 -0600

    Added some routes to homeRoute.js, created login page html and css

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -6,11 +6,62 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
   try {
     const petsData = await Pets.findAll({
-      where:{'user_id': null},
-      include: [User]
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ]
     });
+    // Serialize data so the template can read it
     const pets = petsData.map((pet) => pet.get({ plain: true }));
-    res.render('homepage', { pets, loggedIn: req.session.loggedIn});
+    res.render('homepage', { 
+      pets,
+      loggedIn: req.session.loggedIn});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//each pet page is shown when clicked
+router.get('/pets/:id', async (req, res) => {
+  try {
+    const petsData = await Pets.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const pet = petsData.get({ plain: true });
+
+    res.render('project', {
+      ...pet,
+      loggedIn: req.session.loggedIn
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//gets the profile page with Auth
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: User }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    //checks whether the user is logged in
+    res.render('profile', {
+      ...user,
+      loggedIn: true
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -19,6 +70,7 @@ router.get('/', async (req, res) => {
 // giving you the login and signup route pieces below
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
+    //gets redirected to specific page after logged in
     res.redirect('/dashboard');
     return;
   }
@@ -30,7 +82,6 @@ router.get('/signup', (req, res) => {
     res.redirect('/dashboard');
     return;
   }
-
   res.render('signup');
 });
 
