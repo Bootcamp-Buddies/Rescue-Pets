e9e3f150c86684419e2dff7206c16a3338a155e8
Jacob Scobie <jscobie@focus-solutions.net>
2022-12-29 15:33:02 -0500

    adding home routes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,37 +1,65 @@
 const router = require('express').Router();
 const { Pets, User } = require('../models/');
-const withAuth = require('../utils/auth');
 
-// get all pets for homepage
+// Get all unadopted pets data to display on / homepage - route to homepage
 router.get('/', async (req, res) => {
   try {
-    const petsData = await Pets.findAll({
-      where:{'user_id': null},
-      include: [User]
+    const dbPetData = await Pets.findAll({
+      where: {
+        user_id: null
+      }
     });
-    const pets = petsData.map((pet) => pet.get({ plain: true }));
-    res.render('homepage', { pets, loggedIn: req.session.loggedIn});
+    const pets = dbPetData.map((pet) => pet.get({ plain: true }));
+    res.render('homepage', {
+      pets
+    });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
 
-// giving you the login and signup route pieces below
+// Route for log in page
 router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/dashboard');
+  // If a session exists, redirect the request to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
     return;
   }
   res.render('login');
 });
 
-router.get('/signup', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/dashboard');
-    return;
+// Route for sign up page
+router.get('/signup', (req, res)=> {
+  try {
+    res.render('signup');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
+});
 
-  res.render('signup');
+// Route for a single clicked pet
+router.get('/pets/:id', async (req, res) => {
+  try {
+    const dbPetData = await Pets.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+    const pet = dbPetData.get({ plain: true });
+    res.render('single-pet', {
+      pet,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({error : err, message : 'Something went wrong.'});
+  }
 });
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -2,13 +2,13 @@ const User = require('./User');
 const Pets = require('./Pets');
 
 User.hasMany(Pets, {
-    foreignKey: 'user_id',
-    onDelete: 'cascade'
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
 });
 
 Pets.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'cascade'
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
 });
 
 module.exports = { User, Pets };
