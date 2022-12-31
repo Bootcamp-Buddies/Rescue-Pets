bb61c6afdb21508a2787192675141c0643b1e702
Jacob Scobie <jscobie@focus-solutions.net>
2022-12-31 13:21:51 -0500

    updates to API routes and dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,20 +2,77 @@ const router = require('express').Router();
 const { Pets, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// All pets for User - Dashboard
+// Get all logged in pets for user, which will display on /dashboard page
 router.get('/', withAuth, async (req, res) => {
   try {
-    const petData = await Pets.findAll({
-      where:{'user_id': req.session.userId},
-      include: [User]
+    const dbPetData = await Pets.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'pet_name',
+        'type',
+        'breed',
+        'gender',
+        'age',
+        'traits',
+        'favorite_game',
+        'description'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     });
-    const pets = petData.map((pets) => pets.get({ plain: true }));
-    console.log(pets);
+    const pets = dbPetData.map((pet) => pet.get({ plain: true}));
     res.render('dashboard', {
       pets,
+      logged_in: req.session.logged_in,
+      username: req.session.username
     });
   } catch (err) {
-    res.redirect('login');
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+
+// Route for editing a users pet info from within dashboard
+router.get('/edit/:id', withAuth, async (req, res) => {
+  try {
+    const dbPetData = await Pets.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'pet_name',
+        'type',
+        'breed',
+        'gender',
+        'age',
+        'traits',
+        'favorite_game',
+        'description'
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    });
+    const pets = dbPetData.get({ plain: true});
+    res.render('edit-pet', {
+      pets,
+      logged_in: req.session.logged_in,
+      username: req.session.username
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
   }
 });
 
