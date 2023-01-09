ff32abf31e57bc8df0cab7af50860f45de3a7a18
Jacob Scobie <jscobie@focus-solutions.net>
2023-01-09 14:55:05 -0500

    Adjusted routes and handlebars to solve adopt and logged_in issues

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,7 +11,8 @@ router.get('/', async (req, res) => {
     });
     const pets = dbPetData.map((pet) => pet.get({ plain: true }));
     res.render('homepage', {
-      pets
+      pets,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     console.log(err);
@@ -46,7 +47,9 @@ router.get('/about', (req, res) => {
   //   res.render('about');
   //   return;
   // }
-  res.render('about');
+  res.render('about', {
+    logged_in: req.session.logged_in
+  });
 });
 
 // Route for a single clicked pet
@@ -64,7 +67,8 @@ router.get('/pets/:id', async (req, res) => {
     res.render('pet', {
       pet,
       logged_in: req.session.logged_in,
-      username: req.session.username
+      username: req.session.username,
+      user_id: req.session.user_id
     });
   } catch (err) {
     console.error(err);
